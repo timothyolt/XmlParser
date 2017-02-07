3fd81a6c38c212c157440e85411c356bf9742e19
Timothy Oltjenbruns <timothyolt@gmail.com>
2017-02-07 17:02:57 -0500

    Dummy test

--- xmlParser_implementation/main.cpp
+++ xmlParser_implementation/main.cpp
@@ -1,8 +1,13 @@
 // Copyright 2017 Timothy Oltjenbruns
 
 #include <iostream>
+#include <XmlNode.hpp>
 
 int main(int argCount, char* args[]) {
   std::cout << "Hello XmlParser World!" << std::endl;
+  XmlNode node("name");
+  node.getChildren().reserve(1);
+  node.getChildren()[0] = XmlNode("A");
+  std::cout << node.getChildren()[0].getName() << std::endl;
   return 0;
 }
