5fd5783b0ad62baed2ca9732c58e69cd96ccb7a3
Timothy Oltjenbruns <timothyolt@gmail.com>
2017-02-19 10:16:23 -0500

    More work on parsing XML

--- xmlParser/XmlBuilder.cpp
+++ xmlParser/XmlBuilder.cpp
@@ -1,45 +1,74 @@
 // Copyright 2017 Timothy Oltjenbruns.
 
 #include <vector>
+#include <sstream>
 #include "XmlBuilder.hpp"
 
-class XmlParseException : std::exception {};
+void XmlBuilder::startAttribute(std::string key) {
+
+}
+
+void XmlBuilder::flagAttribute() {
+  // there is whitespace between the attribute key and the assignment operator
+  // make sure that the previous attribute shouldS be flagged
+  if (cursor->getAttributes().size() == 0
+      || !cursor->getAttributes().back().getValue().empty())
+    throw XmlBuilder::XmlParseException();
+  cursor->getAttributes().back().setValue("=");
+  return;
+}
+
+void XmlBuilder::readQuotedValue(std::istream &stream, std::string &buffer) {
+  std::stringstream outStream(buffer);
+  // there is whitespace between the assignment operator and the value
+  // make sure that the previous attribute is ready for value assignment
+  if (cursor->getAttributes().size() == 0
+      || cursor->getAttributes().back().getValue().compare("=") != 0)
+    throw XmlBuilder::XmlParseException();
+  return;
+}
 
 XmlBuilder::XmlBuilder()
-    : root(XmlNode("xml"))
-    , cursor(&root)
-    , cursorOpen(false) { }
+    : root(XmlNode("xml")), cursor(&root), cursorOpen(false) {}
 
 void XmlBuilder::reset() {
   root.setChildren(std::vector<XmlNode>());
   cursorOpen = false;
+  return;
 }
+
+// TODO(timothyolt): GetXml function that ensures a valid tree
 
 // TODO(timothyolt): write looper classes for console, files, stringstream, whatever else
 
-std::istream& operator>>(std::istream& stream, XmlBuilder& builder) {
+std::istream &operator>>(std::istream &stream, XmlBuilder &builder) {
   std::string buffer;
   stream >> buffer;
   switch (buffer[0]) {
-    case '<':
-      // new tag
+    case '<':  // new tag
       switch (buffer[1]) {
-        case '/':
-          // closing tag, no attributes, move cursor
-          break;
-        default:
-          // open tag, begin reading attributes
-          break;
+        case '/': break;  // closing tag, no attributes, move cursor
+        default: break;  // open tag, begin reading attributes
       }
       break;
-    case '/':
-      // end tag, tag has no children, move cursor
-      break;
-    default:
-      // must be accepting attributes
-      if (!builder.cursorOpen)
-        throw XmlParseException();
-
+    case '/': break;  // end tag, tag has no children, move cursor
+    default:  // read attribute segment
+      if (!builder.cursorOpen)  // make sure the cursor should accept attributes
+        throw XmlBuilder::XmlParseException();
+      std::string::size_type assignmentPos(buffer.find("=", 0));
+      std::string::size_type valuePos(buffer.find("\"", 0));
+      switch (buffer[0]) {
+        default:  // begin new attribute with given name
+          builder.startAttribute(buffer.substr(0, assignmentPos));
+          if (assignmentPos == std::string::npos) break;
+        case '=':  // flag attribute that the assignment operator was found
+          builder.flagAttribute();
+          if (valuePos == std::string::npos) break;
+          buffer = buffer.substr(valuePos);
+        case '"':  // begin value, read until end quote
+          builder.readQuotedValue(stream, buffer);
+          break;
+      }
       break;
   }
   return stream;
--- xmlParser/XmlBuilder.hpp
+++ xmlParser/XmlBuilder.hpp
@@ -8,6 +8,7 @@
 
 class XmlBuilder {
  private:
+  class XmlParseException : std::exception {};
   /// \brief  root node of the xml tree in the building process
   XmlNode root;
   /// \brief  Current node to add \p attributes or \p children to
@@ -15,8 +16,16 @@ class XmlBuilder {
   /// \brief  Whether the cursor is "open" or not.
   ///         \p attributes are valid for an open cursor while \p children are valid for a closed one
   bool cursorOpen;
-
-
+  /// \brief      Starts constructing an attribute on the cursor
+  /// \param key  key to give the new attribute
+  void startAttribute(std::string key);
+  /// \brief Flags the current attribute as ready for value assignment
+  void flagAttribute();
+  /// \brief        Reads a stream until it's end quote.
+  ///               Strips out newlines and tabs and processes those escape sequences
+  /// \param stream Input stream to read
+  /// \param buffer Use this buffer to store value in. Useful for setting an initial value.
+  void readQuotedValue(std::istream& stream, std::string& buffer);
 
  public:
   /// \brief  Creates an \p XmlBuilder with a root \p XmlNode named "xml"
@@ -24,11 +33,13 @@ class XmlBuilder {
   /// \brief  Resets the children of the root \p XmlNode
   void reset();
   /// \brief          Atomic stream input
-  ///                 Atomic stream input. Uses a single >> operator internally.
+  ///                 Atomic stream input. Uses a single >> operator internally, unless quotes are encountered,
+  ///                 in that case, the >> operator is called until the quotes are closed. \p readQuotedValue
   ///                 This should be looped and called multiple times until the stream is known to be completed.
   /// \param stream   Input stream reference
   /// \param builder  Destination \p XmlBuilder
   /// \return         Identical stream reference for chaining
+  /// \see            XmlBuilder::readQuotedValue
   friend std::istream& operator>> (std::istream& stream, XmlBuilder& builder);
 };
 
