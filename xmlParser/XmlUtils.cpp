ac513b66a34d61e50085901c03c26dc8411fe6fa
Timothy Oltjenbruns <timothyolt@gmail.com>
2017-02-27 23:46:43 -0500

    Using functionals for tree searching

--- xmlParser/XmlUtils.cpp
+++ xmlParser/XmlUtils.cpp
@@ -1,40 +1,48 @@
 // Copyright 2017 Timothy Oltjenbruns.
 
 #include "XmlUtils.hpp"
-#include <queue>
 
 XmlUtils::Helpers::XmlNodeIndex::XmlNodeIndex(
     const XmlNode *pointer, std::vector<XmlNode>::size_type index)
     : pointer(pointer), index(index) {}
 
-const XmlNode *XmlUtils::searchDf(const XmlNode *root, const XmlAttribute attribute) {
-  std::stack<Helpers::XmlNodeIndex> stack;
-  stack.emplace(root, 0);
-  while (!stack.empty()) {
-    auto cursor(stack.top().pointer);
-    if (stack.top().index == 0 &&
-        std::find(cursor->getConstAttributes().begin(), cursor->getConstAttributes().end(), attribute)
-            != cursor->getConstAttributes().end())
-      return cursor;
-    if (stack.top().index < cursor->getConstChildren().size()) {
-      stack.emplace(&cursor->getConstChildren()[stack.top().index++], 0);
-    }
-    else stack.pop();
-  }
-  return nullptr;
+const XmlNode *XmlUtils::searchDf(const XmlNode *root,
+                                  const XmlAttribute attribute) {
+  return searchDf(root, [attribute](const XmlNode *node) {
+    return std::find(node->getConstAttributes().begin(),
+                     node->getConstAttributes().end(),
+                     attribute) != node->getConstAttributes().end();
+  });
 }
 
-const XmlNode *XmlUtils::searchBf(const XmlNode *root, const XmlAttribute attribute) {
-  std::queue<Helpers::XmlNodeIndex> queue;
-  queue.emplace(root, 0);
-  while (!queue.empty()) {
-    auto cursor(queue.front().pointer);
-    if (std::find(cursor->getConstAttributes().begin(), cursor->getConstAttributes().end(), attribute)
-            != cursor->getConstAttributes().end())
-      return cursor;
-    for (auto i(0); i < cursor->getConstChildren().size(); ++i)
-      queue.emplace(&cursor->getConstChildren()[i], 0);
-    queue.pop();
-  }
-  return nullptr;
+const XmlNode* XmlUtils::searchDf(const XmlNode *root,
+                                     const std::string value) {
+  return searchDf(root, [value](const XmlNode *node) {
+    return value.compare(node->getName()) == 0 ||
+           std::find_if(node->getConstAttributes().begin(),
+                        node->getConstAttributes().end(),
+                        [value](const XmlAttribute &attribute) {
+                          return value.compare(attribute.getValue()) == 0;
+                        }) != node->getConstAttributes().end();
+  });
+}
+
+const XmlNode *XmlUtils::searchBf(const XmlNode *root,
+                                  const XmlAttribute attribute) {
+  return searchBf(root, [attribute](const XmlNode *node) {
+    return std::find(node->getConstAttributes().begin(),
+                     node->getConstAttributes().end(),
+                     attribute) != node->getConstAttributes().end();
+  });
+}
+
+const XmlNode* XmlUtils::searchBf(const XmlNode *root, const std::string value) {
+  return searchBf(root, [value](const XmlNode *node) {
+    return value.compare(node->getName()) == 0 ||
+        std::find_if(node->getConstAttributes().begin(),
+                     node->getConstAttributes().end(),
+                     [value](const XmlAttribute &attribute) {
+                       return value.compare(attribute.getValue()) == 0;
+                     }) != node->getConstAttributes().end();
+  });
 }
--- xmlParser/XmlUtils.hpp
+++ xmlParser/XmlUtils.hpp
@@ -5,6 +5,7 @@
 
 #include <algorithm>
 #include <vector>
+#include <queue>
 #include "XmlNode.hpp"
 
 namespace XmlUtils {
@@ -16,18 +17,57 @@ public:
   XmlNodeIndex(const XmlNode *pointer, std::vector<XmlNode>::size_type index);
 };
 }
-
-/// \brief            Depth-First Search
+/// \brief            Depth-First Search using a Functional to evaluate the node
+/// \param evaluate  Functional used to evaluate the node
+/// \return           Const-pointer to first node matching the functional, or nullptr if not found
+template <typename Functional>
+const XmlNode* searchDf(const XmlNode *root, Functional evaluate) {
+  std::stack<Helpers::XmlNodeIndex> stack;
+  stack.emplace(root, 0);
+  while (!stack.empty()) {
+    auto cursor(stack.top().pointer);
+    if (stack.top().index == 0 && evaluate(cursor))
+      return cursor;
+    if (stack.top().index < cursor->getConstChildren().size()) {
+      stack.emplace(&cursor->getConstChildren()[stack.top().index++], 0);
+    }
+    else stack.pop();
+  }
+  return nullptr;
+}
+/// \brief            Depth-First Search for a node containing a matching attribute
 /// \param attribute  Attribute key/value pair to search for
 /// \return           Const-pointer to first node matching the attribute, or nullptr if not found
-const XmlNode *searchDf(const XmlNode *root,
-                        const XmlAttribute attribute);
-
-/// \brief            Depth-First Search
+const XmlNode* searchDf(const XmlNode *root, const XmlAttribute attribute);
+/// \brief        Depth-First Search for a node with a matching name, or attribute value
+/// \param value  String to compate with node name, and attribute value
+/// \return       Const-pointer to first node matching the attribute, or nullptr if not found
+const XmlNode* searchDf(const XmlNode *root, const std::string value);
+/// \brief            Breadth-First Search using a Functional to evaluate the node
+/// \param evaluate   Functional used to evaluate the node
+/// \return           Const-pointer to first node matching the functional, or nullptr if not found
+template <typename Functional>
+const XmlNode* searchBf(const XmlNode *root, Functional evaluate) {
+  std::queue<Helpers::XmlNodeIndex> queue;
+  queue.emplace(root, 0);
+  while (!queue.empty()) {
+    auto cursor(queue.front().pointer);
+    if (evaluate(cursor))
+      return cursor;
+    for (auto i(0); i < cursor->getConstChildren().size(); ++i)
+      queue.emplace(&cursor->getConstChildren()[i], 0);
+    queue.pop();
+  }
+  return nullptr;
+}
+/// \brief            Breadth-First Search for a node containing a matching attribute
 /// \param attribute  Attribute key/value pair to search for
 /// \return           Const-pointer to first node matching the attribute, or nullptr if not found
-const XmlNode *searchBf(const XmlNode *root,
-                        const XmlAttribute attribute);
+const XmlNode* searchBf(const XmlNode *root, const XmlAttribute attribute);
+/// \brief        Breadth-First Search for a node with a matching name, or attribute value
+/// \param value  String to compate with node name, and attribute value
+/// \return       Const-pointer to first node matching the attribute, or nullptr if not found
+const XmlNode* searchBf(const XmlNode *root, const std::string value);
 };
 
 #endif  // XMLPARSER_XMLUTILS_HPP_
